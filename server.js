bd31fcd0916f6ba81dbe3329dbbcd3a971617f01
IGOR <igornesan@gmail.com>
2020-10-02 21:35:54 +0300

    replace port var

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
 const path = require("path");
-
+const PORT = process.env.PORT || 5000;
 //connect database
 connectDB();
 //init middleware
@@ -21,4 +21,3 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
-const PORT = process.env.PORT || 5000;
